bff0886c3f8d9f835423377fe6cf81e8906aa239
StoutG <gagestout14@gmail.com>
2020-09-05 10:37:53 -0700

    Figured out calculations for votes per candidate + created print statement

--- Pypoll/main.py
+++ Pypoll/main.py
@@ -6,7 +6,7 @@ import csv
 import collections
 from collections import Counter
 
-# Defining are variables
+# Defining our variables
 candidates = []
 votes_per_candidate = []
 
@@ -36,5 +36,26 @@ with open(election_data_csv_path, newline="") as csvfile:
     # Count votes per candidate then append to the created list
     count_candidate = Counter (ordered_list) 
     votes_per_candidate.append(count_candidate.most_common())
+
+    # finding the percentage of votes per candicate (three digit places! '.3f')
+    for item in votes_per_candidate:
+       
+        first = format((item[0][1])*100/(sum(count_candidate.values())),'.3f')
+        second = format((item[1][1])*100/(sum(count_candidate.values())),'.3f')
+        third = format((item[2][1])*100/(sum(count_candidate.values())),'.3f')
+        fourth = format((item[3][1])*100/(sum(count_candidate.values())),'.3f')
+
+# Print the voting analysis, don't forget f string & {}
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes:  {sum(count_candidate.values())}")
+print("-------------------------")
+print(f"{votes_per_candidate[0][0][0]}: {first}% ({votes_per_candidate[0][0][1]})")
+print(f"{votes_per_candidate[0][1][0]}: {second}% ({votes_per_candidate[0][1][1]})")
+print(f"{votes_per_candidate[0][2][0]}: {third}% ({votes_per_candidate[0][2][1]})")
+print(f"{votes_per_candidate[0][3][0]}: {fourth}% ({votes_per_candidate[0][3][1]})")
+print("-------------------------")
+print(f"Winner:  {votes_per_candidate[0][0][0]}")
+print("-------------------------")
 
 
